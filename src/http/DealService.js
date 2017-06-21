223de9f34ca719eeb873d43cea09ea3cb4853a19
mizne <w20054319@126.com>
2017-06-21 20:03:20 +0800

    update

--- src/http/DealService.js
+++ src/http/DealService.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import storage from '@/util/storage'
 
 const getAllFoods = function () {
-  return axios.get(`http://deal.xiaovbao.cn/api/v1/user/Menus/?tenantId=${storage.get('tenantId')}`)
+  return axios.get(`http://deal.xiaovbao.cn/api/v1/user/Menus/?tenantId=${storage.get('tenantId')}&consignee=${storage.get('consignee')}`)
   .then(resp => {
     return resp.data
   })
